0bc90d5fb93112eb37bc7a37fa4c32051308ece6
Rajaseelan Ganeswaran <rajaseelan@gmail.com>
2019-01-16 22:35:05 +0800

    Change Hi to allow multilang

--- testmod.go
+++ testmod.go
@@ -1,10 +1,20 @@
 package testmod
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
-// Hi Returns friendly greeting
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+// Hi Returns friendly greeting in a language lang
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi. %s!", name), nil
+	case "pt":
+		return fmt.Sprintg("Oi, %s!", name), nil
+	default:
+		return "", errors.New("Unknown language")
+	}
 }
 
 
